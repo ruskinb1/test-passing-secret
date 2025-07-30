f52af4dda29a1c18aa102939b58b245ecb0d16d3
Ruskin Singh <66462896+ruskinb1@users.noreply.github.com>
2025-07-30 11:45:32 +0530

    Delete .github/actions/custom-action/index.js

--- .github/actions/custom-action/index.js
+++
@@ -1,8 +0,0 @@
-const core = require('@actions/core');
-
-try {
-  const secretValue = core.getInput('secret_value', { required: true });
-  core.info(`The secret value stored in Repository B is: ${secretValue}`);
-} catch (error) {
-  core.setFailed(`Action failed with error: ${error.message}`);
-}
